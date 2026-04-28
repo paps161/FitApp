60c58a05d41069b528d369ae0ba154d3c3a659ba
papa161 <kchbchbch@gmail.com>
2026-04-28 15:36:33 +0200

    BMI

---
+++ app/src/main/java/com/fitapp/util/BMI.java
@@ -0,0 +1,13 @@
+package com.fitapp.util;
+import java.io.Serializable;
+
+public class BMI implements Serializable{
+    public int kilo;
+    public int groesse;
+    public BMI  (int kilo, int groesse){
+
+
+    }
+
+
+}
