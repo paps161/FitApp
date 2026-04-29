0761d9a7738b86c3e9231b08bf7442d582c08416
papa161 <kchbchbch@gmail.com>
2026-04-29 15:46:04 +0200

    Add MainActivity layout with greeting, BMI definition and formula; comment out EdgeToEdge and WindowInsets in all   activities

--- app/src/main/java/com/fitapp/calculator/ResultActivity.java
+++ app/src/main/java/com/fitapp/calculator/ResultActivity.java
@@ -1,6 +1,7 @@
 package com.fitapp.calculator;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.fitapp.R;
+import com.fitapp.util.BMI;
+
+import java.io.Serializable;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -17,6 +21,15 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_result);
+
+        BMI bmi = (BMI) getIntent().getSerializableExtra("bmi");
+
+        double bmiResult = bmi.berechnen(bmi.kilo, bmi.groesse);
+        String bmiString = String.valueOf(bmiResult);
+
+        TextView tv = findViewById(R.id.textViewResult);
+        tv.setText(bmiString);
+
 
     }
 }
--- app/src/main/java/com/fitapp/util/BMI.java
+++ app/src/main/java/com/fitapp/util/BMI.java
@@ -1,16 +1,19 @@
 package com.fitapp.util;
+import static java.lang.Math.round;
+
 import java.io.Serializable;
 
 public class BMI implements Serializable{
     public double kilo;
     public double groesse;
     public BMI  (double kilo, double groesse){
-
+        this.kilo = kilo;
+        this.groesse = groesse;
 
     }
     public double berechnen (double kilo, double groesse){
-        double groesseM = groesse/100;
-        double bmiresult = kilo/Math.pow(groesseM, 2);
+        double groesseM = groesse/100.0;
+        double bmiresult = Math.round((kilo / Math.pow(groesseM, 2)) * 100.0) / 100.0;
 
         return bmiresult;
     }
