dd58c420de6f85a7e38ea5044305e44cbbad1fca
selina <kchbchbch@gmail.com>
2026-05-09 11:28:48 +0200

    -

--- app/src/main/java/com/fitapp/calculator/EntryActivity.java
+++ app/src/main/java/com/fitapp/calculator/EntryActivity.java
@@ -1,11 +1,6 @@
-// Autor: Selina Weber | Letzte Änderung: 29.04.2026
 package com.fitapp.calculator;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,34 +9,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.fitapp.R;
-import com.fitapp.util.BMI;
 
 public class EntryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+      //  EdgeToEdge.enable(this);
         setContentView(R.layout.activity_entry);
-
-        Button btnS = findViewById(R.id.buttonSubmit);
-        btnS.setOnClickListener(v ->{
-            EditText kiloText = findViewById(R.id.editTextNumberKilo);
-            EditText groesseText = findViewById(R.id.editTextNumberGroesse);
-
-            String kiloString = kiloText.getText().toString();
-            String groesseString = groesseText.getText().toString();
-
-            double kilo = Double.parseDouble(kiloString);
-            double groesse = Double.parseDouble(groesseString);
-
-            BMI bmi = new BMI(kilo, groesse);
-
-            Intent intent = new Intent(this, ResultActivity.class);
-            intent.putExtra("bmi", bmi);
-
-            startActivity(intent);
-        });
-
+      /*  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });*/
     }
 }
--- app/src/main/java/com/fitapp/calculator/ResultActivity.java
+++ app/src/main/java/com/fitapp/calculator/ResultActivity.java
@@ -1,36 +1,25 @@
-// Autor: Selina Weber | Letzte Änderung: 29.04.2026
 package com.fitapp.calculator;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import com.fitapp.R;
-import com.fitapp.util.BMI;
-
-import java.io.Serializable;
 
 public class ResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_result);
-
-        BMI bmi = (BMI) getIntent().getSerializableExtra("bmi");
-
-        double bmiResult = bmi.berechnen(bmi.kilo, bmi.groesse);
-        String bmiString = String.valueOf(bmiResult);
-
-        TextView tv = findViewById(R.id.textViewResult);
-        tv.setText(bmiString);
-
-
+       /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        }); */
     }
 }
